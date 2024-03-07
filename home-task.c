a09f2a0b058a5adfdc29a19dfe76f378edd86ede
emran-hossain <emran.green2037@gmail.com>
2024-03-07 08:14:35 +0600

    added some code

--- home-task.c
+++ home-task.c
@@ -1,44 +1,77 @@
-#include<stdio.h>
-int main()
-{
-    //declare an array to store operation name
-        char *operaion[]={"addition","subtraction","multiplication","division","exit"};
-        int choice;
-        //display menu
-        printf("Choose an operation: \n");
-        printf("1.Addition\n");
-        printf("2.Sustraction\n");
-        printf("3.Multiplication\n");
-        printf("4.Division\n");
-        printf("5.Exit\n");
-        scanf("%d",&choice);
+#include <stdio.h>
 
-        //Check choice is valid
-        //Addition section
-    int a,n,b,sum=0,x[100];
-    printf("Enter number of subject: ");
-    scanf("%d",&n);
-        for(a=0; a<n; a++)
-        {
-            scanf("%d",&x[a]);
-        }
-        for(b=0; b<n; b++)
-        {
-            if(x[b] >= 0 && x[b]<=100)
-            sum=(sum+x[b]);
-        }
-        printf("Addition is: %d\n",sum);
-//subtraction section
+int main() {
+    char *option[] = {"addition", "subtraction", "multiplication", "division", "modulus", "exit"};
+    int select, n, a, b, sum=0, x[100000],mul=1,div;
+    char choice;
 
-        for(a=0; a<n; a++)
-        {
-            scanf("%d",&x[a]);
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Modulus\n");
+        printf("6. Exit\n");
+    do{
+        printf("Select option: ");
+        scanf("%d", &select);
+        switch (select) {
+            case 1: // Addition
+                printf("Enter number of subjects: ");
+                scanf("%d", &n);
+
+                for (a = 0; a < n; a++) {
+                    scanf("%d", &x[a]);
+                    sum += x[a];
+                }
+                printf("Adition is: %d\n", sum);
+                break;
+
+            case 2: // Subtraction
+                printf("Enter two numbers: ");
+                scanf("%d %d", &x[0], &x[1]);
+                printf("Subtraction: %d\n", x[0] - x[1]);
+                break;
+            case 3: // Multiplication
+                printf("Enter number of subjects: ");
+                scanf("%d", &n);
+
+                for (a = 0; a < n; a++) {
+                    scanf("%d", &x[a]);
+                    mul *= x[a];
+                }
+                printf("Multiplication: %d\n", mul);
+                break;
+
+            case 4: // Division
+                printf("Enter two numbers: ");
+                scanf("%d %d", &x[0], &x[1]);
+                if (x[1] != 0){
+                    div = (float)x[0] / x[1];
+                    printf("Division: %.2f\n", div);
+                }
+                else
+                    printf("Error: '0' is not divisible by any number\n");
+                break;
+
+            case 5: // Modulus
+                printf("Enter two numbers: ");
+                scanf("%d %d", &x[0], &x[1]);
+                printf("Modulus: %d\n", x[0] % x[1]);
+                break;
+
+            case 6: // Exit
+                printf("Exit\n");
+                break;
+
+            default:
+                printf("Invalid selection\n");
+                break;
         }
-        for(b=0; b<n; b++)
-        {
-            if(x[b] >= 0 && x[b]<=100)
-            sum=sum*x[b];
-        }
-        printf("Subtraction is: %d\n",sum);
+
+        printf("Do you want to continue? (y/n): ");
+        scanf(" %c", &choice); // Note: Space before %c to consume any leading whitespace
+
+    } while (choice == 'y' || choice == 'Y');
+
     return 0;
 }
