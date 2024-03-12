c50a380c7484b4ca1570c2588630d32450bb7e01
Emran Hossain <92808157+3Emran@users.noreply.github.com>
2024-03-12 13:12:37 +0600

    Add files via upload

---
+++ semester-grade-calculation.c
@@ -0,0 +1,175 @@
+#include<stdio.h>
+
+void first_s();
+void second_s();
+void third_s();
+void fourth_s();
+void five_s();
+void six_s();
+void seven_s();
+void eight_s();
+void nine_s();
+void ten_s();
+void eleven_s();
+void twelve_s();
+int main()
+{
+    int n;
+    printf("Enter semester no: ");
+    scanf("%d",&n);
+
+switch(n)
+{
+case 1:{
+    first_s();
+    break;
+}
+{
+case 2:
+    second_s();
+    break;
+}
+case 3:{
+    third_s();
+    break;
+}
+case 4:{
+    fourth_s();
+    break;
+}
+case 5:{
+    five_s();
+    break;
+}
+case 6:{
+    six_s();
+    break;
+}
+case 7:{
+    seven_s();
+    break;
+}
+case 8:{
+    eight_s();
+    break;
+}
+case 9:{
+    nine_s();
+    break;
+}
+case 10:{
+    ten_s();
+    break;
+}
+case 11:{
+    eleven_s();
+    break;
+}
+case 12:{
+    twelve_s();
+    break;
+}
+default:{
+    printf("Error\n");
+    break;
+    }
+    return 0;
+}
+}
+void first_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    printf("Enter course grade: \n");
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*3+s5*1.5)/13.5;
+    printf("GPA = %.2f\n",gpa);
+
+}
+void second_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void third_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void fourth_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void five_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void six_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void seven_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void eight_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void nine_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void ten_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void eleven_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
+void twelve_s()
+{
+    float s1,s2,s3,s4,s5,gpa;
+    scanf("%f %f %f %f %f",&s1,&s2,&s3,&s4,&s5);
+    gpa=(s1*3+s2*3+s3*3+s4*4+s5*1.5)/13.5;
+    printf("%.2f\n",gpa);
+
+}
